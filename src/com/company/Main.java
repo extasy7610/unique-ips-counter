00dbbdaaa0729698080a79e43ec6a09efb5f3777
Fedya Lavrentyev <lavr@x-cart.com>
2022-02-28 16:23:23 +0400

    add ip generator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        generateIps();
+    }
+
+    private static void generateIps() {
+        for (int i = 1; i < 40001; i++) {
+            System.out.print(
+                    getRandomNumber(0, 999)
+            );
+
+            if ((i % 4) == 0) {
+                System.out.println();
+            } else {
+                System.out.print(".");
+            }
+        }
+    }
+
+    private static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
